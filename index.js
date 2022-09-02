b87a2d93cd536de6ed6f79e32ed576946348fdbb
Naveen <Naveen.b@technovert.com>
2022-09-02 17:46:39 +0530

    Completed Challenge

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 var express = require('express');
 var cors = require('cors');
+let bodyParser = require('body-parser')
+let multer = require('multer');
+let path = require('path')
+let fs = require('fs')
 require('dotenv').config()
 
 var app = express();
+let fileInfo =null;
 
+const storage = multer.diskStorage({
+  destination:(req,file,cb)=> {
+    cb(null,'uploads')
+  },
+  filename:(req,file,cb)=>{
+    fileInfo={
+      name:file.originalname,
+      type:file.mimetype,
+      size:file.size
+    };
+    console.log(file.mimetype)
+    cb(null,file.originalname);
+  }
+})
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 
+const upload = multer({storage:storage});
+
+app.post("/api/fileanalyse",upload.single('upfile') ,(req,res)=> {
+  fileInfo.size = fs.statSync(`./uploads/${fileInfo.name}`).size
+  console.log(fileInfo)
+  res.json(fileInfo)
+})
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
